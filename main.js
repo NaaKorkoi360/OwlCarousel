1f83bfb81baca137b0de9be2c02de08e53cee16a
Naakorkoi360 <Naakorkoiokine10@gmail.com>
2023-10-06 02:11:06 -0700

    First Time

---
+++ main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
